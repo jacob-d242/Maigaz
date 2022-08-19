059d4ed29c0d1ddb70bf2bada4e469fdaa8091e6
jacob-d242 <jacobmoracha@gmail.com>
2022-08-20 00:24:49 +0300

    updating home screen

--- src/screens/home/Favourites.js
+++ src/screens/home/Favourites.js
@@ -1,12 +1,18 @@
 import React from 'react'
-import { Text, View } from "react-native";
-
+import { Image, Text, View,StyleSheet } from "react-native";
+import { COLORS, IMAGES } from "../../constants/Index"
+import Header from "../../components/Header"
+import NoFavourite from './components/NoFav';
 export default function Favourites() {
     return ( 
         <View>
-            <Text>
-                Favourites
-            </Text>
+            <Header/>
+           <NoFavourite/>
         </View>
     )
 }
+
+
+const styles = StyleSheet.create({
+   
+})
