88f6233f03ff72e48f6333858e345a35e8abcc16
jacob-d242 <jacobmoracha@gmail.com>
2022-12-01 23:58:59 +0300

    creating data to use for cart

---
+++ src/global/Data.js
@@ -0,0 +1,38 @@
+export const ProductData = [
+    {
+        name: "SeaGas",
+        id: "0"
+    }, 
+    {
+        name: "Total",
+        id: "1"
+    }, 
+    {
+        name: "Mpishi",
+        id: "2"
+    }, 
+    {
+        name: "Hashi",
+        id: "3"
+    }, 
+    {
+        name: "K-Gas",
+        id: "4"
+    }, 
+    {
+        name: "Vivo",
+        id: "6"
+    }, 
+    {
+        name: "Rubis",
+        id: "7"
+    },
+    {
+        name: "Ola Ebnergy",
+        id: "8"
+    },
+    {
+        name: "Pro Gas",
+        id: "9"
+    } 
+]
