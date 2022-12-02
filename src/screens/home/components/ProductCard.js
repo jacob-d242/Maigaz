12e032cd6d1c587e814f9da26bad704a6baecadf
jacob-d242 <jacobmoracha@gmail.com>
2022-12-02 23:30:25 +0300

    updating product card

--- src/screens/home/components/ProductCard.js
+++ src/screens/home/components/ProductCard.js
@@ -1,42 +1,8 @@
 import React, { useState,useEffect } from 'react'
 import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
-import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
-import NumericInput from 'react-native-numeric-input'
 import { COLORS, IMAGES } from '../../../constants/Index'
-import SelectList from 'react-native-dropdown-select-list'
-import firestore  from '@react-native-firebase/firestore'
-import { useNavigation } from '@react-navigation/native'
-export default function ProductCard() {
-    const [selected, setSelected] = useState({brand:'',category:'',subcategory:''});
-    const [formData,setFormData] = useState();
-    const [category, setCategory] = useState();
-    const [subcategory, setSubCategory] = useState();
-    const [brand,setBrand] = useState();
-    const navigation = useNavigation();
-    const vendor = [
-        { key :'SA' , value :'SeaGas'},
-        { key :'TT' , value :'Total'},
-        { key :'MP' , value :'Mpishi'},
-    ]
-    const categories = [
-        { key :'A' , value :'6'},
-        { key :'B' , value :'8'},
-        { key :'C' , value :'12'},
-        { key :'D' , value :'15'},
-        { key :'E' , value :'16'}
-    ]
-    const subCategories = {
-        "A": [{ key: '1', value: '1200' }],
-        "B": [{ key: '2', value: '1800' }],
-        "C": [{ key: '3', value: '3200' }],
-        "D": [{ key: '4', value: '3800' }],
-        "E": [{ key: '5', value: '4800' }],
-    }
-    const orderHandler = () => {
-        console.log(selected)
-       {navigation.navigate('OrderAdress',{formData:selected})}
-    }
-    return (
+export default function ProductCard(){ 
+return (
         <View style={styles.cartProduct}>
             <Image
                 resizeMode="contain"
@@ -45,13 +11,7 @@ export default function ProductCard() {
             />
             <View>
                 <Text>13 Kg Gas Clylinder</Text>
-           </View>
-            {/*<TouchableOpacity style={styles.btn}
-                onPress={orderHandler}
-            >
-                <Text style={{fontSize:28,color:"#FFF",fontWeight:'Inter-Bold'}}>Next</Text>
-            </TouchableOpacity>*/}
-            
+           </View>            
         </View>
     )
 }
@@ -61,8 +21,8 @@ const styles = StyleSheet.create({
         backgroundColor: '#FFF',
         borderRadius: 35,
         flexDirection: 'column',
-        height: 350,
-        width:'80%',
+        height: 250,
+        width:150,
         alignItems: 'center',
         marginHorizontal:'10%'
     },
@@ -84,7 +44,6 @@ const styles = StyleSheet.create({
         fontSize: 22,
         color: '#FFF',
         height: 45,
-        //borderWidth: 1,
         borderRadius: 13,
         backgroundColor:COLORS.primary,
         width: '40%',
