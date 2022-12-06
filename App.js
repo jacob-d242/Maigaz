7ec7bf28f4901cbfc31b299bbc546b1cb0a6ce93
jacob-d242 <jacobmoracha@gmail.com>
2022-12-06 18:07:57 +0300

    updating home screen

--- App.js
+++ App.js
@@ -4,11 +4,15 @@ import { Provider } from 'react-redux'
 import thunk from 'redux-thunk';
 import RootNavigation from './src/navigation/RootNavigation';
 import reducers from './src/screens/store/index';
+import { NativeBaseProvider } from "native-base";
 export default function App() {
   const store = createStore(reducers, applyMiddleware(thunk));
   return (
-    <Provider store={store} >
-     <RootNavigation/>
-    </Provider>
+    <NativeBaseProvider>
+      <Provider store={store} >
+      <RootNavigation/>
+      </Provider>
+
+    </NativeBaseProvider>
   );
 };
